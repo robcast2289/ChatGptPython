b596a5a3d16564228698cba33e39c1ddc05e50ab
Roberto <tonocastrom@gmail.com>
2024-04-10 11:10:30 -0600

    Cambios a chatgpt

--- chatgpt.py
+++ chatgpt.py
@@ -2,23 +2,35 @@ from openai import OpenAI
 
 client = OpenAI(api_key = "")
 
+conversation_history = []
+
 while True:
 
-    prompt = input("\nPregunta: ")
+    print("Pregunta: ")
+    prompt = input()
 
     if prompt == "exit":
         break
 
+    conversation_history.append(
+        {
+            "role":"user",
+            "content":prompt
+        }
+    )
+
     completion = client.chat.completions.create(
         model="gpt-3.5-turbo",
         #prompt=prompt,
-        messages=[
-            {
-                "role":"user",
-                "content":prompt
-            }
-        ],
+        messages=conversation_history,
         max_tokens=500
     )
+
+    conversation_history.append(
+        {
+            "role":"assistant",
+            "content":completion.choices[0].message.content
+        }
+    )
     
-    print(f"Respuesta: {completion.choices[0].message.content}")
+    print(f"Respuesta: {conversation_history[-1]['content']}")
